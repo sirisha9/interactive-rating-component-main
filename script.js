5da217a7cbb4424cd16adeb478fa8a0ebb7e33c7
sirisha akurathi <akurathi.sirisha@gmail.com>
2023-08-30 22:06:37 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-    const num = document.querySelectorAll("#num");
+    const num = document.querySelectorAll(".num");
     const btn = document.getElementById("subBtn");
     const thankyou = document.querySelector(".thank-you");
     const rating= document.querySelector(".userValue");
@@ -9,12 +9,12 @@
         num[i].addEventListener("click",()=>{
             let ratings = num[i].innerHTML;
 
-            for(let i=0;i<num.length;i++){
+            /*for(let i=0;i<num.length;i++){
                num[i].classList.contains('selected');
                num[i].classList.remove('selected');
 
             }
-            num[i].classList.add('selected');
+            num[i].classList.add('selected');*/
             btn.addEventListener('click',()=>{
                 mainContainer.style.display = 'none';
                 thankyou.style.display = 'block';
